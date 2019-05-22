053dc0af73c607e85f42dbd026e90d5aa7921a18
ChristianMacDonald <chrismacd412@gmail.com>
2019-05-22 00:27:54 -0400

    Relocated dummy-data.js and added SearchBar

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <input placeholder="Search" />
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
